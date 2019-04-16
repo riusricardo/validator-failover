820f22c5363209ad204aca9af5278eea092030f5
Ricardo Rius <ricardo@rius.info>
2019-04-16 13:10:51 -0500

    Improved the basic template

--- basic/BasicValidatorNode.py
+++ basic/BasicValidatorNode.py
@@ -3,9 +3,6 @@ from AWSIoTPythonSDK.MQTTLib import AWSIoTMQTTShadowClient
 from pystemd.systemd1 import Unit
 import time
 
-# A random programmatic shadow client ID.
-SHADOW_CLIENT = "ShadowNodeClient"
-
 # The unique hostname that &IoT; generated for 
 # this device.
 HOST_NAME = "yourhostname-ats.iot.us-east-1.amazonaws.com"
@@ -25,7 +22,7 @@ PRIVATE_KEY = "yourkeyid-private.pem.key"
 CERT_FILE = "yourkeyid-certificate.pem.crt.txt"
 
 # A programmatic shadow handler name prefix.
-SHADOW_HANDLER = "ValidatorNode"
+SHADOW_HANDLER = "ValidatorNodeThing"
 
 unit = Unit(b'polkadot-validator.service')
 # Automatically called whenever the shadow is updated.
@@ -38,34 +35,33 @@ def myShadowUpdateCallback(payload, responseStatus, token):
   print("token = " + token)
 
 # Create, configure, and connect a shadow client.
-myShadowClient = AWSIoTMQTTShadowClient(SHADOW_CLIENT)
-myShadowClient.configureEndpoint(HOST_NAME, 8883)
-myShadowClient.configureCredentials(ROOT_CA, PRIVATE_KEY,
+shadowClient = AWSIoTMQTTShadowClient(SHADOW_HANDLER)
+shadowClient.configureEndpoint(HOST_NAME, 8883)
+shadowClient.configureCredentials(ROOT_CA, PRIVATE_KEY,
   CERT_FILE)
-myShadowClient.configureConnectDisconnectTimeout(10)
-myShadowClient.configureMQTTOperationTimeout(5)
-myShadowClient.connect()
+shadowClient.configureConnectDisconnectTimeout(10)
+shadowClient.configureMQTTOperationTimeout(5)
+shadowClient.connect()
 
 # Create a programmatic representation of the shadow.
-myDeviceShadow = myShadowClient.createShadowHandlerWithName(
-  SHADOW_HANDLER, True)
+deviceShadow = shadowClient.createShadowHandlerWithName(SHADOW_HANDLER, True)
 
 # Initialize
 unit.load()
-status = 'active'
+status = 'unknown'
 
 # To stop running this script, press Ctrl+C.
 while True:
   
-  prev_status = status
+  prevStatus = status
   status = unit.Unit.ActiveState.decode('utf-8')
   
-  if prev_status != status:
+  if prevStatus != status:
       msg = '{"state":{"reported":{"status":"%(data)s"}}}' % { 'data' : status}
   else:
-      msg = '{"state":{"desired":{"status":"%(data)s"}}}' % { 'data' : prev_status}
+      msg = '{"state":{"desired":{"status":"%(data)s"}}}' % { 'data' : prevStatus}
 
-  myDeviceShadow.shadowUpdate(msg, myShadowUpdateCallback, 5)
+  deviceShadow.shadowUpdate(msg, myShadowUpdateCallback, 5)
   
   # Wait for this test value to be added.
   time.sleep(2)
